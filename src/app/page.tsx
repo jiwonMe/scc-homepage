828236c01324d812a4f6dac33334cf5e94b1f89c
jiwonMe <park@jiwon.me>
2024-11-11 05:56:43 +0900

    temp name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import Navbar from "../components/navbar";
-import { CTAButton } from "../components/ui/cta-button";
+import Navbar from "@/components/navbar";
+import { CTAButton } from "@/components/ui/cta-button";
 import { FeatureSection } from "../components/sections/feature-section";
 
 export default function Home() {
